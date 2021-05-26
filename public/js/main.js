4746164eac5215dab49c2ae38e7d6d017cc804a6
Alanderson Tomaiz <alan_tomaiz@hotmail.com>
2021-05-26 17:54:27 -0300

    Inserção no banco (MySQL)

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,5 @@
+const items = [];
+
 const populateStates = async () => {
   const response = await fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados`)
     .then(response => response.json());
@@ -24,6 +26,16 @@ const populateCities = async () => {
 
 const handleItemSelect = (item) => {
   item.classList.toggle('selected');
+
+  const data = item.dataset.type;
+  const index = items.findIndex(string => string === data);
+
+  if (index >= 0) {
+    items.splice(index, 1);
+    return;
+  }
+
+  items.push(data);
 }
 
 const getSelectState = () => {
@@ -36,6 +48,33 @@ const getSelectCity = () => {
 
 const getCollectItems = () => {
   return document.querySelectorAll('.wrapper-items li')
+}
+
+const handleCreatePoint = async (button) => {
+  const formData = { items: items.join(', ') };
+  const form = document.querySelectorAll('#create-form input, #create-form select');
+
+  form.forEach(elem => {
+    formData[elem.getAttribute('name')] = elem.value;
+  });
+
+  button.disabled = true;
+  button.innerHTML = 'Aguarde...';
+
+  const request = {
+    method: 'POST',
+    body: JSON.stringify(formData),
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+  }
+
+  const response = await fetch('/create/', request)
+    .then(response => response.json());
+
+  button.disabled = false;
+  button.innerHTML = 'Cadastrar ponto de coleta';
 }
 
 populateStates();
--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
+const mysql = require('./database/connect');
 const server = express();
+
+server.use(express.json());
 
 // assets
 server.use(express.static('public'));
@@ -19,6 +22,23 @@ server.get('/', (request, response) => {
 // Apresentação da page Create
 server.get('/create', (request, response) => {
   return response.render('create.html');
+});
+
+// Cadastro de pontos
+server.post('/create', (request, response) => {
+  const data = Object.values(request.body).map(item => item);
+
+  mysql.query(`
+  INSERT INTO points (items, name, address, number, state, city) VALUES (?, ?, ?, ?, ?, ?)
+  `, data, (error) => {
+    if (error) {
+      console.log(error);
+
+      return response.send({ status: 'error', message: 'Deu ruim.' });
+    }
+
+    return response.send({ status: 'success' });
+  });
 });
 
 // Apresentação da page Points
